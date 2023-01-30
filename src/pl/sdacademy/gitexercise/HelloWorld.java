757f0b236f26a56392407a9df178a0f6e5b66c75
STJimmmy <tox85@mail.ru>
2023-01-30 19:23:33 +0100

    Commit 1

--- src/pl/sdacademy/gitexercise/HelloWorld.java
+++ src/pl/sdacademy/gitexercise/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("hello world, Lukasz Witkowski");
+        System.out.println("tu Anton Baykov");
 
     }
 }
