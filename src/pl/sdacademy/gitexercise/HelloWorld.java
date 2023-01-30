ada215e94f71234a3920dd1c6ca0331727718001
lukasz.witkowski <lwitkowski@vaudoise-uaa.org>
2023-01-30 18:06:37 +0000

    #Task324: (przykladowa nazwa) - Nowy plik HelloWorld wyswietlajacy nazwisko prowadzacego - zmiany w gitingore

---
+++ src/pl/sdacademy/gitexercise/HelloWorld.java
@@ -0,0 +1,9 @@
+package pl.sdacademy.gitexercise;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("hello world, Lukasz Witkowski");
+
+    }
+}
